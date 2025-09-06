c56f260c2189aa79e186a35f627193de8e4ede73
Abhishek Patidar <imababhishek@gmail.com>
2025-09-06 14:19:40 +0530

    last

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      preview: {
+        allowedHosts: ['dermaticsai.onrender.com']
       }
     };
 });
